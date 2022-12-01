44a9939cf2ba135f688a6d6e57c2eb00c9ccaa80
Oleg Krashenkin <k125629@ya.ru>
2022-12-01 18:18:58 +0300

    Выполнил ДЗ к первому уроку

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = 'переменная'
+const screens = 'переменная'
+const screenPrice = 'переменная'
+const rollback = 'переменная'
+const fullPrice = 'переменная'
+const adaptive = 'переменная'
+
+alert('Модальное окно')
+
+console.log('Любой текст')
