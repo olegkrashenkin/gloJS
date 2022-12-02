6cca5af0b86d0248ea5c1e2a3133411effaf6ab4
Oleg Krashenkin <k125629@ya.ru>
2022-12-02 22:02:10 +0300

    Правки к ДЗ

--- script.js
+++ script.js
@@ -5,24 +5,13 @@ const rollback = 12
 const fullPrice = 1000
 const adaptive = true
 
-console.log(
-    // Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
-    `title - ${title} - ` + typeof title +
-    `\nfullPrice - ${fullPrice} - ` + typeof fullPrice +
-    `\nadaptive - ${adaptive} - ` + typeof adaptive +
-
-    // Вывести в консоль длину строки из переменной screens
-    `\nДлина строки из переменной screens - ${screens.length}` +
-
-    // Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани”
-    // и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
-    `\nСтоимость верстки экранов ${screenPrice} рублей/долларов/гривен/юани` +
-    `\nСтоимость разработки сайта ${fullPrice} рублей/долларов/гривен/юани` +
-
-    // Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
-    `\n` + screens.toLowerCase().split(', ') +
-
-    // Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
-    `\n` + (fullPrice * (rollback/100))
+console.log(`title - ${title} - ${typeof title}
+fullPrice - ${fullPrice} - ${typeof fullPrice}
+adaptive - ${adaptive} - ${typeof adaptive}
+Длина строки из переменной screens - ${screens.length}
+Стоимость верстки экранов ${screenPrice} рублей
+Стоимость разработки сайта ${fullPrice} рублей
+${screens.toLowerCase().split(', ')}
+${(fullPrice * (rollback/100))}`
 )
 
