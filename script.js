568abd6010f33c0084b0291f07be4acdfd0929f5
Oleg Krashenkin <k125629@ya.ru>
2022-12-05 21:17:34 +0300

    Мелкие правки

--- script.js
+++ script.js
@@ -2,19 +2,23 @@
 const title = prompt('Как называется ваш проект?', 'Проект')
 const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные')
 const screenPrice = +prompt('Сколько будет стоить данная работа?', '12000')
-const adaptive = prompt('Нужен ли адаптив на сайте?', 'Да/Нет') === 'Да' ? true : false
+const adaptive = confirm('Нужен ли адаптив на сайте?')
 const service1 = prompt('Какой дополнительный тип услуги нужен?')
 const servicePrice1 = +prompt('Сколько это будет стоить?', '1000')
 const service2 = prompt('Какой дополнительный тип услуги нужен?')
 const servicePrice2 = +prompt('Сколько это будет стоить?', '1000')
-// let service1, service2, servicePrice1, servicePrice2
-// for (let i = 1; i < 3; i++) {
-//     eval('service' + i + ' = ' + 'prompt(\'Какой дополнительный тип услуги нужен?\')')
-//     eval('servicePrice' + i + ' = ' + prompt('Сколько это будет стоить?', '1000'))
-// }
 const fullPrice = screenPrice + servicePrice1 + servicePrice2
 const rollback = 12
 const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)))
+console.log(`title - ${title} - ${typeof title}
+fullPrice - ${fullPrice} - ${typeof fullPrice}
+adaptive - ${adaptive} - ${typeof adaptive}
+Длина строки из переменной screens - ${screens.length}
+Стоимость верстки экранов ${screenPrice} рублей
+Стоимость разработки сайта ${fullPrice} рублей
+${screens.toLowerCase().split(', ')}
+${(fullPrice * (rollback/100))}`
+)
 if (fullPrice <= 0){
     console.log('Что то пошло не так')
 }else if (fullPrice <= 15000){
@@ -24,15 +28,7 @@ if (fullPrice <= 0){
 }else {
     console.log('Даем скидку в 10%')
 }
-// console.log(`title - ${title} - ${typeof title}
-// fullPrice - ${fullPrice} - ${typeof fullPrice}
-// adaptive - ${adaptive} - ${typeof adaptive}
-// Длина строки из переменной screens - ${screens.length}
-// Стоимость верстки экранов ${screenPrice} рублей
-// Стоимость разработки сайта ${fullPrice} рублей
-// ${screens.toLowerCase().split(', ')}
-// ${(fullPrice * (rollback/100))}`
-// )
+
 
 
 
