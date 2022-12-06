585b423cee85b52ac99ee2f4ccea70486fc3ad4c
Oleg Krashenkin <k125629@ya.ru>
2022-12-06 18:39:50 +0300

    ДЗ к 4 уроку

--- script.js
+++ script.js
@@ -1,30 +1,64 @@
 'use strict'
-const title = prompt('Как называется ваш проект?', 'Проект')
-const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные')
-const screenPrice = +prompt('Сколько будет стоить данная работа?', '12000')
-const adaptive = confirm('Нужен ли адаптив на сайте?')
-const service1 = prompt('Какой дополнительный тип услуги нужен?')
-const servicePrice1 = +prompt('Сколько это будет стоить?', '1000')
-const service2 = prompt('Какой дополнительный тип услуги нужен?')
-const servicePrice2 = +prompt('Сколько это будет стоить?', '1000')
-const fullPrice = screenPrice + servicePrice1 + servicePrice2
-const rollback = 12
-const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)))
-console.log(`title - ${title} - ${typeof title}
-fullPrice - ${fullPrice} - ${typeof fullPrice}
-adaptive - ${adaptive} - ${typeof adaptive}
-Длина строки из переменной screens - ${screens.length}
-Стоимость верстки экранов ${screenPrice} рублей
-Стоимость разработки сайта ${fullPrice} рублей
-${screens.toLowerCase().split(', ')}
-${(fullPrice * (rollback/100))}`
-)
-if (fullPrice <= 0){
-    console.log('Что то пошло не так')
-}else if (fullPrice <= 15000){
-    console.log('Скидка не предусмотрена')
-}else if (fullPrice <= 30000){
-    console.log('Даем скидку в 5%')
-}else {
-    console.log('Даем скидку в 10%')
+
+
+let title = prompt('Как называется ваш проект?', ' КаЛьКулятор Верстки')
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные')
+let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000')
+let adaptive = confirm('Нужен ли адаптив на сайте?')
+let service1 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice1 = +prompt('Сколько это будет стоить?', '1000')
+let service2 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice2 = +prompt('Сколько это будет стоить?', '1000')
+let rollback = 12
+let allServicePrices, fullPrice, servicePercentPrice
+
+
+const getAllServicePrices = function (...prices) {
+    let result = 0
+    for (const price of prices) {
+        result += price
+    }
+    return result
 }
+
+function getFullPrice (screenPrice, allServicePrices) {
+    return screenPrice + allServicePrices
+}
+
+function getTitle (title) {
+    return title.trim()[0].toUpperCase() + title.slice(1).toLowerCase()
+}
+
+function getServicePercentPrices (fullPrice, rollback) {
+    return Math.ceil(fullPrice - (fullPrice * (rollback / 100)))
+}
+
+const showTypeOf = function (variable) {
+    return `${variable} - ${typeof variable}`
+}
+
+const getRollbackMessage = function (price) {
+    if (price <= 0) {
+        return 'Что то пошло не так'
+    } else if (price <= 15000) {
+        return 'Скидка не предусмотрена'
+    } else if (price <= 30000) {
+        return 'Даем скидку в 5%'
+    } else {
+        return 'Даем скидку в 10%'
+    }
+}
+
+
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2)
+fullPrice = getFullPrice(screenPrice, allServicePrices)
+servicePercentPrice = getServicePercentPrices(fullPrice, rollback)
+title = getTitle(title)
+
+
+console.log(showTypeOf(title))
+console.log(showTypeOf(fullPrice))
+console.log(showTypeOf(adaptive))
+console.log(screens)
+console.log(getRollbackMessage(fullPrice))
+console.log(getServicePercentPrices(fullPrice, rollback))
